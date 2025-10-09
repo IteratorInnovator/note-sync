6d6624ea0230c2469361a95ec0c41a09cb384b31
jaeltay <jael.tay.2024@smu.edu.sg>
2025-10-09 17:11:28 +0800

    Update firestore.js
    Attempted to do getUser() function

--- src/services/utils/firestore.js
+++ src/services/utils/firestore.js
@@ -1,11 +1,32 @@
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { app } from "../..";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC0XvKd14qKqDFrrgXsSraIf1biANk5geA",
+  authDomain: "wad2-44a13.firebaseapp.com",
+  projectId: "wad2-44a13",
+  storageBucket: "wad2-44a13.firebasestorage.app",
+  messagingSenderId: "216595758621",
+  appId: "1:216595758621:web:b7e801ddd3f3c13ee310f4",
+  measurementId: "G-M03J250RMZ"
+};
+
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export const createUser = () => {
-
-}
+export const createUser = async (user) => {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      ...user,
+      createdAt: serverTimestamp(), // auto timestamp
+    });
+    console.log("User created with ID:", docRef.id);
+    return docRef.id;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw error;
+  }
+};
 
 export const getVideosByUserId = (uid) => {
     
