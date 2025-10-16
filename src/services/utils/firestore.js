049b6c252156a18594d6fcd3dc2e2b41113e4e4f
jaeltay <jael.tay.2024@smu.edu.sg>
2025-10-17 00:00:59 +0800

    Update firestore.js
    CRUD functions for notes, added for delete video function as well, which supposedly if you delete videos, notes within are not automatically deleted so have to account for that as well.

--- src/services/utils/firestore.js
+++ src/services/utils/firestore.js
@@ -47,9 +47,77 @@ export const addVideo = async (uid, videoId, title, channelTitle, thumbnail) => 
   });
 };
 
-/**
- * Delete a video document by ID
- */
-export const deleteVideo = (videoId, uid) => {
-  // (to be implemented if needed)
+export const deleteVideo = async (uid, videoId) => {
+  try {
+    // will need to remove all notes first before deleting video as firebase will not auto remove when deleting video.
+    const notesColRef = collection(db, "users", uid, "videos", videoId, "notes");
+    const notesSnapshot = await getDocs(notesColRef);
+    const deleteNotesPromises = notesSnapshot.docs.map((noteDoc) => deleteDoc(noteDoc.ref));
+    await Promise.all(deleteNotesPromises);
+
+    // deleting the vid
+    const videoRef = doc(db, "users", uid, "videos", videoId);
+    await deleteDoc(videoRef);
+
+    return videoId;
+  } 
+  
+  catch (error) {
+    throw error;
+  }
+};
+
+export const getNotesByVideoId = async (uid, videoId) => {
+  const notesCol = collection(db, "users", uid, "videos", videoId, "notes");
+  const q = query(notesCol, orderBy("timeSec", "asc")); // optional ordering
+  const snap = await getDocs(q);
+  return snap.docs.map(doc => ({ noteId: doc.id, ...doc.data() }));
+};
+
+export const createNote = async (uid, videoId, content, timeSec) => {
+  try {
+    const notesCol = collection(db, "users", uid, "videos", videoId, "notes");
+
+    const docRef = await addDoc(notesCol, {
+      timeSec,
+      content,
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+    });
+
+    return docRef.id;
+  } 
+  
+  catch (error) {
+    throw error;
+  }
+};
+
+export const updateNote = async (uid, videoId, noteId, newContent) => {
+  try {
+    const ref = doc(db, "users", uid, "videos", videoId, "notes", noteId);
+
+    await updateDoc(ref, {
+      newContent,
+      updatedAt: serverTimestamp(),
+    });
+
+    return noteId;
+  } 
+  
+  catch (error) {
+    throw error;
+  }
+};
+
+export const deleteNote = async (uid, videoId, noteId) => {
+  try {
+    const ref = doc(db, "users", uid, "videos", videoId, "notes", noteId);
+    await deleteDoc(ref);
+    return noteId;
+  } 
+  
+  catch (error) {
+    throw error;
+  }
 };
